dbad5b7699069b4272f9a77223fafcd708429053
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2019-12-15 21:21:05 +0200

    Add include section to pyproject.toml Add '-f', '--format' option to gendiff -h

--- gendiff.py
+++ gendiff.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument("first_file", help="Input path to first file")
     parser.add_argument("second_file", help="input path to second file")
+    parser.add_argument("-f", "--format", help="set format of output")
     parser.parse_args()
 
 
