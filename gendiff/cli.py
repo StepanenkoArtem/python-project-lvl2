1a7dbaec199c11cbb6c21dfa417d04ee3b45ac16
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2019-12-23 15:07:47 +0200

    refactored generate_diff"

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,7 +1,7 @@
 def output(text, view):
     if view == 'json':
-        print("{open_bracket}\n{text}\n{close_bracket}".format(
-            open_bracket="{",
+        print("{open_bracket}{text}{close_bracket}".format(
+            open_bracket="{\n",
             text=text,
             close_bracket="}")
         )
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -8,48 +8,30 @@ parser = argparse.ArgumentParser(
 )
 
 
-def get_abspath(path):
-    return os.path.abspath(path)
-
-
 def get_keys(dictionary):
-    return list(dictionary.keys())
+    return set(dictionary.keys())
 
 
-def common_keys_differense(first_dict, second_dict):
-    result = ""
-    first_dict_keys = set(get_keys(first_dict))
-    second_dict_keys = set(get_keys(second_dict))
-    common_keys = first_dict_keys.intersection(second_dict_keys)
-    for key in common_keys:
-        if first_dict[key] != second_dict[key]:
-            result = result + "    + {}: {}\n    - {}: {}\n".format(
-                key, first_dict[key],
-                key, second_dict[key],
-            )
-    return result
+def append_to_result(key, value, sign=" "):
+    return '{sign} {key}: {value}\n'.format(sign=sign, key=key, value=value)
 
 
-def get_difference_between(first_dict, second_dict):
-    result = ""
-    first_dict_keys = set(get_keys(first_dict))
-    second_dict_keys = set(get_keys(second_dict))
-    different_keys = first_dict_keys.difference(second_dict_keys)
-    for key in different_keys:
-        result = result + "    - {}: {}\n".format(
-            key, first_dict[key]
-        )
-    different_keys = second_dict_keys.difference(first_dict_keys)
-    for key in different_keys:
-        result = result + "    + {}: {}\n".format(
-            key, second_dict[key]
-        )
-    return result
-
-
-def generate_diff(before, after):
-    before_dict = json.load(open(get_abspath(before)))
-    after_dict = json.load(open(get_abspath(after)))
-    diff = common_keys_differense(before_dict, after_dict) + \
-           get_difference_between(before_dict, after_dict)
+def generate_diff(before_file, after_file):
+    diff = ""
+    before = json.load(open(os.path.abspath(before_file)))
+    after = json.load(open(os.path.abspath(after_file)))
+    before_keys = get_keys(before)
+    after_keys = get_keys(after)
+    all_keys = before_keys.union(after_keys)
+    for key in all_keys:
+        if key in before_keys and key in after_keys:
+            if before[key] == after[key]:
+                diff = diff + append_to_result(key, before[key])
+            else:
+                diff = diff + append_to_result(key, before[key], sign='-')
+                diff = diff + append_to_result(key, after[key], sign='+')
+        elif key in before_keys and key not in after_keys:
+            diff = diff + append_to_result(key, before[key], sign='-')
+        elif key not in before_keys and key in after_keys:
+            diff = diff + append_to_result(key, after[key], sign='+')
     return diff
