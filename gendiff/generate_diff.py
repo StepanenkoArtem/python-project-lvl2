3ffe3c2b8f258d63140fb87047c7924ddc42c9dc
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2019-12-26 12:03:04 +0200

    Add get_path() tests

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -16,10 +16,18 @@ def append_to_result(key, value, sign=" "):
     return '{sign} {key}: {value}\n'.format(sign=sign, key=key, value=value)
 
 
+def get_path(path):
+    if "~" in path:
+        path = os.path.expanduser(path)
+    path = os.path.abspath(os.path.normpath(path))
+    print(path)
+    return path
+
+
 def generate_diff(before_file, after_file):
     diff = ""
-    before = json.load(open(os.path.abspath(before_file)))
-    after = json.load(open(os.path.abspath(after_file)))
+    before = json.load(open(get_path(before_file)))
+    after = json.load(open(get_path(after_file)))
     before_keys = get_keys(before)
     after_keys = get_keys(after)
     all_keys = before_keys.union(after_keys)
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,11 +1,21 @@
-from gendiff.generate_diff import generate_diff
+from gendiff.generate_diff import generate_diff, get_path
 import os
-import pytest
 
 
 def test_generate_diff():
     check_file = open(os.path.join(os.getcwd(), "tests/fixtures/diff"), 'r')
     check_text = check_file.read()
-    checking_string = generate_diff('tests/fixtures/testfiles/test1.json',
-                                    'tests/fixtures/testfiles/test2.json')
+    checking_string = generate_diff(
+        'tests/fixtures/testfiles/test1.json',
+        'tests/fixtures/testfiles/test2.json')
     assert set(check_text.split('\n')) == set(checking_string.split('\n'))
+
+
+def test_get_path():
+    control_path = "/home/chief/pyprojects/python-project-lvl2/tests/" \
+                   "fixtures/testfiles/test2.json"
+    paths = open(
+        os.path.join(os.getcwd(), "tests/fixtures/paths.txt"), 'r'
+    )
+    for path in paths:
+        assert get_path(path.rstrip()) == control_path
