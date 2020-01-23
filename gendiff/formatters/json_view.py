6017cc0d67dc42cd4235b5725af358306abdb832
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-01-23 13:24:04 +0200

    refactored lines appending to result

--- gendiff/formatters/json_view.py
+++ gendiff/formatters/json_view.py
@@ -1,37 +1,45 @@
-
-
-_STATUS = "status"
-_VALUE = "value"
-_REMOVED_VALUE = "removed value"
-_ADDED_VALUE = "added value"
-
-
-# avaliable statuses
-MOD = "modified"
-ADD = "added"
-REM = "removed"
-UNC = "unchanged"
-
-
 def generate_view(data):
     result = {}
-    for key, value in sorted(data.items()):
+    for key, value in data.items():
         if isinstance(value, dict):
             result.update({key: generate_view(value)})
         elif isinstance(value, tuple):
             if value[0] and value[1]:
-                result.update({key: {_STATUS: MOD,
-                                     _REMOVED_VALUE: value[0],
-                                     _ADDED_VALUE: value[1]
-                                     }}
-                              )
+                result.update(
+                    {
+                        key: {
+                            "status": "modified",
+                            "removed value": value[0],
+                            "added value": value[1]
+                        }
+                    }
+                )
             if not value[0]:
-                result.update({key: {_STATUS: ADD, _VALUE: value[1]}})
+                result.update(
+                    {
+                        key: {
+                            "status": "added",
+                            "value": value[1]
+                        }
+                    }
+                )
             if not value[1]:
-                result.update({key: {_STATUS: REM, _VALUE: value[0]}})
+                result.update(
+                    {
+                        key: {
+                            "status": "removed",
+                            "value": value[0]
+                        }
+                    }
+                )
         else:
             result.update(
-                {key: {_STATUS: UNC, _VALUE: value}}
+                {
+                    key: {
+                        "status": "unchanged",
+                        "value": value
+                    }
+                }
             )
     return result
 
