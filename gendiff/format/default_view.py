763179063c8e5687e07d344fb75ab028f6179e20
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-01-29 11:26:26 +0200

    sign definition moved to consts

--- gendiff/format/default_view.py
+++ gendiff/format/default_view.py
@@ -2,20 +2,23 @@ from gendiff.py_to_json import convert
 from gendiff.status import *
 
 # Indentation settings
-INITIAL_INDENT = 0
-DEFAULT_INDENT = 4
-FILLER = ' '
-BACKSHIFT = 2
+_INITIAL_INDENT = 0
+_DEFAULT_INDENT = 4
+_FILLER = ' '
+_BACKSHIFT = 2
 
-format_line = '{sign}{key}: {value}\n'.format
+# Tokens
+_ADD = '+'
+_REM = '-'
 
 
-def generate_view(data, indent=INITIAL_INDENT):
+def generate_view(data, indent=_INITIAL_INDENT):
+    format_line = '{sign}{key}: {value}\n'.format
     lines = ["{\n"]
 
     def add_line(_value, sign=" "):
-        left = (shift - BACKSHIFT) * FILLER
-        right = FILLER
+        left = (shift - _BACKSHIFT) * _FILLER
+        right = _FILLER
         formatted_sign = '{}{}{}'.format(left, sign, right)
         line = format_line(
             sign=formatted_sign,
@@ -25,63 +28,37 @@ def generate_view(data, indent=INITIAL_INDENT):
         return lines.append(line)
 
     for key, param in sorted(data.items()):
-        shift = indent + DEFAULT_INDENT
+        shift = indent + _DEFAULT_INDENT
         if isinstance(param, dict):
-            add_line(
-                _value=generate_view(param, shift)
-            )
+            add_line(generate_view(param, shift))
         elif isinstance(param, tuple):
             status, value = param
             if status == REMOVED:
                 if isinstance(value, dict):
-                    add_line(
-                        _value=generate_view(value, shift),
-                        sign="-"
-                    )
+                    add_line(generate_view(value, shift), _REM)
                 else:
-                    add_line(
-                        _value=convert(value),
-                        sign="-"
-                    )
+                    add_line(convert(value), _REM)
             if status == ADDED:
                 if isinstance(value, dict):
-                    add_line(
-                        _value=generate_view(value, shift),
-                        sign="+"
-                    )
+                    add_line(generate_view(value, shift), _ADD)
                 else:
-                    add_line(
-                        _value=convert(value),
-                        sign="+"
-                    )
+                    add_line(convert(value), _ADD)
             if status == MODIFIED:
                 value_before = value[0]
                 value_after = value[1]
                 if isinstance(value_before, dict):
-                    add_line(
-                        _value=generate_view(value_before, shift),
-                        sign="-"
-                    )
+                    add_line(generate_view(value_before, shift), _REM)
                 else:
-                    add_line(
-                        _value=convert(value_before),
-                        sign="-"
-                    )
+                    add_line(convert(value_before), _REM)
                 if isinstance(value_after, dict):
-                    add_line(
-                        _value=generate_view(value_after, shift),
-                        sign="+"
-                    )
+                    add_line(generate_view(value_after, shift), _ADD)
                 else:
-                    add_line(
-                        _value=convert(value_after),
-                        sign="+"
-                    )
+                    add_line(convert(value_after), _ADD)
         else:
             add_line(
                 _value=convert(param)
             )
-    lines.append("{}{}".format(indent * FILLER, "}"))
+    lines.append("{}{}".format(indent * _FILLER, "}"))
     return "".join(lines)
 
 
