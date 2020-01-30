988e62caa56c32db7ff7b34dd98b95916fcb991e
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2020-01-30 10:29:47 +0200

    _get_del_items() and _get_add_items() was unified to single function names _get_diff_items()

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -23,30 +23,21 @@ parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.add_argument(
     '-f', '--format',
-#   choices=format.FORMATTERS,
     default=format.DEFAULT,
     help='set format of output',
     type=formatter,
 )
 
 
-def _get_removed_items(diff, before_data, after_data):
-    removed_items = {}
-    removed_keys = before_data.keys() - after_data.keys()
-    for key in removed_keys:
-        removed_items.update({key: (REMOVED, before_data[key])})
-    return diff.update(removed_items)
+def _get_diff_items(minuend, subtrahend, status):
+    deff_items = {}
+    diff_item_keys = minuend.keys() - subtrahend.keys()
+    for key in diff_item_keys:
+        deff_items.update({key: (status, minuend[key])})
+    return deff_items
 
 
-def _get_added_items(diff, before_data, after_data):
-    added_items = {}
-    added_keys = after_data.keys() - before_data.keys()
-    for key in added_keys:
-        added_items.update({key: (ADDED, after_data[key])})
-    return diff.update(added_items)
-
-
-def _get_modified_items(diff, before_data, after_data):
+def _get_modified_items(before_data, after_data):
     changed = {}
     common_keys = before_data.keys() & after_data.keys()
     for key in common_keys:
@@ -68,15 +59,14 @@ def _get_modified_items(diff, before_data, after_data):
                 item_after)
                     )
         changed[key] = compared
-    return diff.update(changed)
+    return changed
 
 
 def compare(before_data, after_data):
     internal_diff = {}
-    for update in (_get_added_items,
-                   _get_modified_items,
-                   _get_removed_items):
-        update(internal_diff, before_data, after_data)
+    internal_diff.update(_get_diff_items(before_data, after_data, REMOVED))
+    internal_diff.update(_get_diff_items(after_data, before_data, ADDED))
+    internal_diff.update(_get_modified_items(before_data, after_data))
     return internal_diff
 
 
